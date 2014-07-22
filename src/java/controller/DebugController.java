293a9c93023f05eee937ee063706353d04023afe
Ryan Boykin <ryancb06@gmail.com>
2014-07-22 18:37:57 -0500

    Added some JavaDoc

--- src/java/controller/DebugController.java
+++ src/java/controller/DebugController.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package controller;
 
 import ejb.MovieEJB;
@@ -28,6 +22,9 @@ public class DebugController {
     @EJB private MovieEJB movieEJB;
     @EJB private ShowtimeEJB showtimeEJB;
     
+    /**
+     *  Clears all entries from the database.
+     */
     public void clearDB(){
         theaterEJB.clear();
         movieEJB.clear();
--- src/java/controller/MovieController.java
+++ src/java/controller/MovieController.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package controller;
 
 import ejb.MovieEJB;
@@ -29,18 +23,33 @@ public class MovieController {
     protected Movie movie = new Movie();
     protected List<Movie> movieList = new ArrayList<>();
 
+    /**
+     * Getter for this.movie
+     * @return Current instance of movie
+     */
     public Movie getMovie() {
         return movie;
     }
-
+    
+    /**
+     * Setter for this.movie
+     * @param movie
+     */
     public void setMovie(Movie movie) {
         this.movie = movie;
     }
-
+    
+    /**
+     * @return A list of all movies from database.
+     */
     public List<Movie> getMovieList() {
         return movieEJB.getAll();
     }
     
+    /**
+     * Looks at get param "id" and finds movie of that ID if one exists
+     * @return Instance of Movie if param "id" is valid otherwise null
+     */
     public Movie getMovieByParamId() {
         FacesContext context = FacesContext.getCurrentInstance();
         Map<String, String> paramMap = context.getExternalContext().getRequestParameterMap();
@@ -54,10 +63,18 @@ public class MovieController {
         return null;
     }
 
+    /**
+     * Setter for this.movieList
+     * @param movieList
+     */
     public void setMovieList(List<Movie> movieList) {
         this.movieList = movieList;
     }
-
+    
+    /**
+     * Adds movie via MovieEJB.
+     * @return "movie_list.xhtml"
+     */
     public String addMovie() {
         movie = movieEJB.add(movie);
         return "movie_list.xhtml";
