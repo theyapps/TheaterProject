ec0e6ba470e22acb919e29775b445cf91dc67f3d
Ryan Boykin <ryancb06@gmail.com>
2014-07-25 16:54:33 -0500

    Added TicketController

---
+++ src/java/controller/TicketController.java
@@ -0,0 +1,18 @@
+/*
+ * 
+ */
+
+package controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author ryan
+ */
+@Named(value = "ticketController")
+@RequestScoped
+public class TicketController {
+    
+}
